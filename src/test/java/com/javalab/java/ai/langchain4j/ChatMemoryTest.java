d9906b6fcc8dfd4ef44b7386d3d959a549a3b3b5
kyrielin <kyrielin666@gmail.com>
2025-05-07 01:00:34 +0800

    feat:添加配置，智能体初构造

--- src/test/java/com/javalab/java/ai/langchain4j/ChatMemoryTest.java
+++ src/test/java/com/javalab/java/ai/langchain4j/ChatMemoryTest.java
@@ -1,10 +1,14 @@
 package com.javalab.java.ai.langchain4j;
 
 import com.javalab.java.ai.langchain4j.assistant.Assistant;
+import com.javalab.java.ai.langchain4j.assistant.MemoryChatAssistant;
 import dev.langchain4j.community.model.dashscope.QwenChatModel;
 import dev.langchain4j.data.message.AiMessage;
 import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.response.ChatResponse;
+import dev.langchain4j.service.AiServices;
+import dev.langchain4j.service.spring.AiService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,6 +23,8 @@ public class ChatMemoryTest {
     private Assistant assistant;
     @Autowired
     private QwenChatModel qwenChatModel;
+    @Autowired
+    private MemoryChatAssistant memoryChatAssistant;
 
     @Test
     public void testChatMemory() {
@@ -42,4 +48,26 @@ public class ChatMemoryTest {
         System.out.println(aiMessage2.text());
     }
 
+    @Test
+    public void testChatMemory3() {
+
+        MessageWindowChatMemory chatMemory = MessageWindowChatMemory.withMaxMessages(10);
+        Assistant assistant = AiServices.builder(Assistant.class)
+                .chatLanguageModel(qwenChatModel)
+                .chatMemory(chatMemory)
+                .build();
+
+        String answer1 = assistant.chat("我是环环");
+        System.out.println(answer1);
+        String answer2 = assistant.chat("你知道我是谁吗？");
+        System.out.println(answer2);
+    }
+
+    @Test
+    public void testChatMemory4() {
+        String chat = memoryChatAssistant.chat("我是环环");
+        System.out.println(chat);
+        String chat2 = memoryChatAssistant.chat("你知道我是谁吗？");
+        System.out.println(chat2);
+    }
 }
