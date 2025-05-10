71fd923ba40128a1e78dd020f4d854b12b0f4444
kyrielin <kyrielin666@gmail.com>
2025-05-10 23:34:42 +0800

    feat:修改配置回滚

--- src/test/java/com/javalab/java/ai/langchain4j/LLMTest.java
+++ src/test/java/com/javalab/java/ai/langchain4j/LLMTest.java
@@ -3,6 +3,8 @@ package com.javalab.java.ai.langchain4j;
 import dev.langchain4j.community.model.dashscope.QwenChatModel;
 import dev.langchain4j.community.model.dashscope.WanxImageModel;
 import dev.langchain4j.data.image.Image;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.ollama.OllamaChatModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.langchain4j.model.output.Response;
 import org.junit.jupiter.api.Test;
@@ -29,30 +31,39 @@ public class LLMTest {
     }
 
     @Autowired
-    private OpenAiChatModel openAiChatModel;
+    private OpenAiChatModel  openAiChatModel;
+
+
     @Test
     public void testLLM2() {
-        String answer = openAiChatModel.chat("我是谁？");
+        String answer = openAiChatModel.chat("你是谁？");
+        System.out.println(answer);
+    }
+
+    @Autowired
+    private OllamaChatModel ollamaChatModel;
+    @Test
+    public void testLLM3() {
+        String answer = ollamaChatModel.chat("你是谁？");
         System.out.println(answer);
     }
 
     @Autowired
     private QwenChatModel qwenChatModel;
-
     @Test
-    public void testLLM3() {
-        String answer = qwenChatModel.chat("我是谁？");
+    public void testLLM4() {
+        String answer = qwenChatModel.chat("你是谁？");
         System.out.println(answer);
     }
 
     @Test
-    public void testLLM4() {
+    public void testLLM5() {
         WanxImageModel wanxImageModel = WanxImageModel
                 .builder()
-                .modelName("wanx2.1-t2i-turbo")
-                .apiKey(System.getenv("WANX.API_KEY"))
+                .modelName("wanx2.1-t2i-turbo" )
+                .apiKey(System.getenv( "DASH_SCOPE_API_KEY" ))
                 .build();
-        Response<Image> response = wanxImageModel.generate("");
+        Response<Image> response = wanxImageModel.generate("奇幻森林精灵： 在一片弥漫着轻柔薄雾的古老森林深处，阳光透过茂密枝叶洒下金色光斑。一位身材娇小长着透明薄翼的精灵少女站在一朵硕大的蘑菇上。她有着海藻般的绿色长发，发间点缀着蓝色的小花，皮肤泛着珍珠般的微光。身上穿着由翠绿树叶和白色藤蔓编织而成的连衣裙，手中捧着一颗散发着柔和光芒的水晶球，周围环绕着五彩斑斓的蝴蝶，脚下是铺满苔藓的地面，蘑菇和蕨类植物丛生，营造出神秘而梦幻的氛围。" );
         URI url = response.content().url();
         System.out.println(url);
     }
