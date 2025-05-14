a27768614f79511eeb6ddf31f73b1efd2e92caae
kyrielin <kyrielin666@gmail.com>
2025-05-15 00:02:25 +0800

    fix:智能客服1.0，初步完成。

--- src/main/java/com/javalab/java/ai/langchain4j/config/XiaozhiAgentConfig.java
+++ src/main/java/com/javalab/java/ai/langchain4j/config/XiaozhiAgentConfig.java
@@ -1,12 +1,24 @@
 package com.javalab.java.ai.langchain4j.config;
 
 import com.javalab.java.ai.langchain4j.store.MongoChatMemoryStore;
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
+import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.memory.chat.ChatMemoryProvider;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
-import dev.langchain4j.store.memory.chat.ChatMemoryStore;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.rag.content.retriever.ContentRetriever;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
+import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
+import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class XiaozhiAgentConfig {
@@ -22,5 +34,37 @@ public class XiaozhiAgentConfig {
                     .maxMessages(20)
                     .chatMemoryStore(mongoChatMemoryStore)
                     .build();
+    }
+
+//    @Bean
+//     ContentRetriever contentRetrieverXiaozhi() {
+//        //使用FileSystemDocumentLoader读取指定目录下的知识库文档
+//        // 并使用默认的文档解析器对文档进行解析
+//        Document document1 = FileSystemDocumentLoader.loadDocument("/Users/kyrielin/Downloads/资料/knowledge/医院信息.md");
+//        Document document2 = FileSystemDocumentLoader.loadDocument("/Users/kyrielin/Downloads/资料/knowledge/科室信息.md");
+//        Document document3 = FileSystemDocumentLoader.loadDocument("/Users/kyrielin/Downloads/资料/knowledge/神经内科.md");
+//        List<Document> documents = Arrays.asList(document1, document2, document3);
+//        //使用内存向量存储
+//        InMemoryEmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
+//        //使用默认的文档分割器
+//        EmbeddingStoreIngestor.ingest(documents, embeddingStore);
+//        // 从嵌入存储（EmbeddingStore）里检索和查询内容相关的信息
+//        return EmbeddingStoreContentRetriever.from(embeddingStore);
+//        }
+
+    @Autowired
+    private EmbeddingStore embeddingStore;
+    @Autowired
+    private EmbeddingModel embeddingModel;
+
+    @Bean
+    ContentRetriever contentRetrieverXiaozhiPincone(){
+        //创建一个EmbeddingStoreContentRetriever对象，用于嵌入存储检索
+        return EmbeddingStoreContentRetriever.builder()
+                .embeddingModel(embeddingModel)
+                .embeddingStore(embeddingStore)
+                .maxResults(1)
+                .minScore(0.8)
+                .build();
     }
 }
